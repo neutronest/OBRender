e6524c78a8e28fd6f76b8630af3c0ea8591ec2fb
neutronest <neutronest@gmail.com>
2017-02-10 03:22:27 +0900

    dev

--- bookmark.ml
+++ bookmark.ml
@@ -1,6 +1,6 @@
 open Yojson.Basic
 open Yojson.Basic.Util
-
+open String
 type bookmark_type = Url | Folder
 
 type bookmark_item = {
@@ -13,20 +13,30 @@ type bookmark_item = {
   children: bookmark_item list
 }
 
+let wrap_to_string json_with_null =
+  match json_with_null with
+  `String s -> s
+  | _ -> "-42"
+
+let wrap_to_list json_with_null =
+  match json_with_null with
+    `List l -> l
+  | _ -> []
+
 let rec new_bookmark_item json_data =
-  let timestamp_ = json_data |> member "date_added" |> to_string in
-  let id_ = json_data |> member "id" |> to_string in
-  let meta_info_ = json_data |> member "meta_info" |> to_string in
+  let timestamp_ = json_data |> member "date_added" |> wrap_to_string in
+  let id_ = json_data |> member "id" |> wrap_to_string  in
+  let meta_info_ = json_data |> member "meta_info" |> wrap_to_string in
   let item_type_ =
-    let type_str = json_data |> member "type" |> to_string in
+    let type_str = json_data |> member "type" |> wrap_to_string in
     match type_str with
     | "url" -> Url
     | "folder" -> Folder
     | _ -> Url in
-  let name_ = json_data |> member "name" |> to_string in
-  let url_ = json_data |> member "url" |> to_string in
+  let name_ = json_data |> member "name" |> wrap_to_string in
+  let url_ = json_data |> member "url" |> wrap_to_string  in
   let children_ =
-    let children_json = json_data |> member "children" |> to_list in
+    let children_json = json_data |> member "children"  |> wrap_to_list in
     List.map new_bookmark_item children_json in
   {
     timestamp = timestamp_;
@@ -37,3 +47,9 @@ let rec new_bookmark_item json_data =
     url = url_;
     children = children_;
   }
+
+let rec print_bookmark (item: bookmark_item) =
+  let rec go tab_length item = 
+    print_endline ((String.make tab_length '\t')^item.name);
+    List.iter (go (tab_length+1)) item.children;
+  in go 0 item
--- main.ml
+++ main.ml
@@ -6,7 +6,10 @@ let () =
   let bookmark_json = "Bookmarks.json" in
   let json_data = Yojson.Basic.from_file bookmark_json in
   let open Yojson.Basic.Util in
-  let checksum = json_data |> member "checksum" |> to_string in
+  let checksum = json_data |> member "checksum" |> wrap_to_string in
+  let bookmark_json = json_data |> member "roots" |> member "bookmark_bar" |> new_bookmark_item  in
   print_endline "hello world!";
   print_endline checksum;
+  print_bookmark bookmark_json;
+
  
